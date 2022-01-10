e549e6adad17a6c43875d610f1b3270c7c3bc715
ITworkonline <1130172267@qq.com>
2022-01-09 19:55:46 -0700

    Make image be a imageurl

--- TwitterClone/ViewModels/AuthViewModel.swift
+++ TwitterClone/ViewModels/AuthViewModel.swift
@@ -12,14 +12,29 @@ class AuthViewModel: ObservableObject {
         
     }
     func registerUser(email: String, password: String, username: String, fullname: String, profileImage: UIImage) {
-        
-        Auth.auth().createUser(withEmail: email, password: password){
-            result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+                                                    
+        storageRef.putData(imageData, metadata: nil) { _, error in
             if let error = error {
-                print("DEBUG: Error \(error.localizedDescription)")
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
                 return
             }
-            print("DEBUG: Successfully signed up user...")
+            
+            storageRef.downloadURL {url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                Auth.auth().createUser(withEmail: email, password: password){
+                    result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email, "username": username, "fullname": fullname, "profileImageUrl": profileImageUrl, "uid":user.uid]
+                }
+            }
         }
     }
 }
